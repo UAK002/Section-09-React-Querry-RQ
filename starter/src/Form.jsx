b8b4c2ef84804b48134835f7332746cdfa774c9e
UAK002 <126017094+UAK002@users.noreply.github.com>
2023-08-01 05:31:35 +0530

    311. Create Task - Initial Setup

--- starter/src/Form.jsx
+++ starter/src/Form.jsx
@@ -1,22 +1,33 @@
+import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
+import customFetch from './utils';
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
 
+  // const result = useMutation({
+  const { mutate: createTask, isLoading } = useMutation({
+    mutationFn: (taskTitle) => customFetch.post('/', { title: taskTitle }),
+  });
+
+  // console.log(result);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    createTask(newItemName);
   };
   return (
     <form onSubmit={handleSubmit}>
       <h4>task bud</h4>
-      <div className='form-control'>
+      <div className="form-control">
         <input
-          type='text '
-          className='form-input'
+          type="text "
+          className="form-input"
           value={newItemName}
           onChange={(event) => setNewItemName(event.target.value)}
         />
-        <button type='submit' className='btn'>
+        {/* <button type="submit" className="btn"> */}
+        <button type="submit" className="btn" disabled={isLoading}>
           add task
         </button>
       </div>
--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,8 +1,38 @@
 import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+
+import { useQuery } from '@tanstack/react-query';
+import customFetch from './utils';
+
+const Items = () => {
+  const { isLoading, data, isError, error } = useQuery({
+    queryKey: ['tasks'],
+    queryFn: async () => {
+      // const { data } = await customFetch.get('/something');
+      const { data } = await customFetch.get('/');
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <p style={{ marginTop: '1rem' }}>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p style={{ marginTop: '1rem' }}> There was an error...</p>;
+  }
+  // console.log(error);
+  // if (isError) {
+  //   return <p style={{ marginTop: '1rem' }}>{error.message}</p>;
+  // }
+  // if (isError) {
+  //   return <p style={{ marginTop: '1rem' }}>{error.response.data}</p>;
+  // }
+
+  // console.log(data);
+
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
